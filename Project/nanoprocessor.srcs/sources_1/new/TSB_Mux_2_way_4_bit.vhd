871793f9f6abb16d01a4808a89fcc54e454bc22a
Kevin Fernando <94424408+JudeKFdo@users.noreply.github.com>
2023-06-04 01:31:16 +0530

    Add 2-way-4-bit mux and update nanoprocessor.xpr
    Add 2-way-4-bit mux and update nanoprocessor.xpr

--- Project/nanoprocessor.srcs/sources_1/new/TSB_Mux_2_way_4_bit.vhd
+++ Project/nanoprocessor.srcs/sources_1/new/TSB_Mux_2_way_4_bit.vhd
@@ -32,12 +32,38 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity TSB_Mux_2_way_4_bit is
---  Port ( );
+PORT (
+    I0 : in STD_LOGIC_VECTOR (3 downto 0);
+    I1 : in STD_LOGIC_VECTOR (3 downto 0);
+    Sel : in STD_LOGIC;
+    Y : out STD_LOGIC_VECTOR (3 downto 0)
+);
 end TSB_Mux_2_way_4_bit;
 
 architecture Behavioral of TSB_Mux_2_way_4_bit is
 
+COMPONENT TSB_4_Bit 
+PORT (
+    I : in STD_LOGIC_VECTOR (3 downto 0);
+    Sel : in STD_LOGIC;
+    Y : out STD_LOGIC_VECTOR (3 downto 0)
+);
+END COMPONENT;
+
 begin
 
+TSB_4_Bit_0 : TSB_4_Bit
+port map (
+	I => I0,
+	Sel => Sel,
+	Y => Y
+);
+
+TSB_4_Bit_1 : TSB_4_Bit
+port map (
+	I => I1,
+	Sel => Sel,
+	Y => Y
+);
 
 end Behavioral;
