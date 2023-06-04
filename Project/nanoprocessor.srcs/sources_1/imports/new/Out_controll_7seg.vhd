d2891dd7d4735cffb34b0b67c57d756354d1180a
roshanemoraes <moraes.git@gmail.com>
2023-06-04 16:22:40 +0530

    Rename Out_controll_7seg to Out_control_7seg
    Rename Out_controll_7seg to Out_control_7seg

--- Project/nanoprocessor.srcs/sources_1/imports/new/Out_controll_7seg.vhd
+++ Project/nanoprocessor.srcs/sources_1/imports/new/Out_controll_7seg.vhd
@@ -4,13 +4,13 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 
-entity Out_controll_7seg is
+entity Out_control_7seg is
     Port ( R7_out : in STD_LOGIC_VECTOR (3 downto 0);
            Anode_out : out STD_LOGIC_VECTOR (3 downto 0);
            Cathode_out : out STD_LOGIC_VECTOR (6 downto 0));
-end Out_controll_7seg;
+end Out_control_7seg;
 
-architecture Behavioral of Out_controll_7seg is
+architecture Behavioral of Out_control_7seg is
    
     component LUT_16_7
         Port ( Addr : in STD_LOGIC_VECTOR (3 downto 0);
